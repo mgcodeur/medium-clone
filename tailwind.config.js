f5402cd31f62fe3749b0eb97b6c3905dbd7e56a2
mgcodeur <mgcodeur@gmail.com>
2025-01-25 07:16:15 +0300

    chore(tailwind): migrate to v4, setup Husky and Commitlint
    This update brings the project to Tailwind CSS version 4, unlocking new features and improvements.
    It also introduces Husky for managing pre-commit hooks and Commitlint to help enforce consistent
    commit message standards.

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./**/*.{html,js}', '!node_modules/**'],
-  darkMode: ['selector', '[data-theme="dark"]'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
